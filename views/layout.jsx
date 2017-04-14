c0b38b355c737794af3e3c399651f87b52a703f5
Allen Dean <allen.dean@us.ibm.com>
2017-04-14 08:50:41 -0400

    Update link to docs
    After conversion to Markdown, file name changed.
    
    - Link in readme was going to API reference, not main docs. Updated to point to
    latest Markdown-sourced output

--- views/layout.jsx
+++ views/layout.jsx
@@ -35,7 +35,7 @@ function Layout(props) {
         <Jumbotron
           serviceName="Discovery"
           repository="https://github.com/watson-developer-cloud/discovery-nodejs"
-          documentation="http://www.ibm.com/watson/developercloud/doc/discovery"
+          documentation="http://www.ibm.com/watson/developercloud/doc/discovery/index.html"
           apiReference="http://www.ibm.com/watson/developercloud/discovery/api"
           startInBluemix="https://console.ng.bluemix.net/registration/?target=/catalog/services/discovery/"
           version="GA"
