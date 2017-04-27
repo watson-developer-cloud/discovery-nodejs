56ac21fde8645645eef6bc5757367758d9c38452
Zachary-Dernar <Zachary.M.Dernar@ibm.com>
2017-04-27 18:11:17 -0400

    Removes "No Results" gif
    We don't own the rights to Jurassic Park

--- views/NoResults/index.jsx
+++ views/NoResults/index.jsx
@@ -2,7 +2,6 @@ import React, { PropTypes } from 'react';
 
 const NoResults = (props) => (
   <div style={{ width: '100%', textAlign: 'center', marginBottom: '5rem' }}>
-    <img alt="No Results" src="http://metrouk2.files.wordpress.com/2014/08/results6.gif?w=851&h=463" />
     <p className="base--h2">
       There are no results for: {props.query ? props.query.text : '' }
     </p>
