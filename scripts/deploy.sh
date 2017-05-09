02282a1d858b55b05d1a7183ebca07f723d6287b
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2017-05-09 14:29:23 -0400

    Fix deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,13 @@ source $BLUEMIX_API_KEY_FILE
 BLUEMIX_API_URL="https://api.ng.bluemix.net"
 BLUEMIX_ORG="WatsonPlatformServices"
 BLUEMIX_SPACE="demos"
+BLUEMIX_APP_NAME="discovery-news-demo-staging"
+BLUEMIX_MAINIFEST_FILE="manifest.$1.yml"
 
 ./cf login -a $BLUEMIX_API_URL -o $BLUEMIX_ORG -s $BLUEMIX_SPACE -u apikey -p $BLUEMIX_API_KEY
-./cf zero-downtime-push discovery-news-demo -f manifest.yml
+
+if [ "$1" == "production"]; then
+  BLUEMIX_APP_NAME="discovery-news-demo"
+fi
+
+./cf zero-downtime-push $BLUEMIX_APP_NAME -f $BLUEMIX_MAINIFEST_FILE
