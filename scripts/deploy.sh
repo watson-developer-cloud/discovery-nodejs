30b0515fb71a024c3710a24ea9e44793087f682e
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2017-05-09 02:22:13 -0400

    Fix cf deployment scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,5 +8,5 @@ BLUEMIX_API_URL="https://api.ng.bluemix.net"
 BLUEMIX_ORG="WatsonPlatformServices"
 BLUEMIX_SPACE="demos"
 
-cf login -a $BLUEMIX_API_URL -o $BLUEMIX_ORG -s $BLUEMIX_SPACE -u apikey -p $BLUEMIX_API_KEY
-cf zero-downtime-push discovery-news-demo -f manifest.yml
+./cf login -a $BLUEMIX_API_URL -o $BLUEMIX_ORG -s $BLUEMIX_SPACE -u apikey -p $BLUEMIX_API_KEY
+./cf zero-downtime-push discovery-news-demo -f manifest.yml
