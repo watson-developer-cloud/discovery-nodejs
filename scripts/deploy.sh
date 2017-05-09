3f9663d24f72ddd852bbb95ddf05ade3c7870ab3
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2017-05-09 14:48:06 -0400

    Move cloudfoundry deploy into script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,10 +10,13 @@ BLUEMIX_SPACE="demos"
 BLUEMIX_APP_NAME="discovery-news-demo-staging"
 BLUEMIX_MAINIFEST_FILE="manifest.$1.yml"
 
-cf login -a $BLUEMIX_API_URL -o $BLUEMIX_ORG -s $BLUEMIX_SPACE -u apikey -p $BLUEMIX_API_KEY
+wget 'https://cli.run.pivotal.io/stable?release=linux64-binary&source=github' -qO cf-linux-amd64.tgz && tar -zxvf cf-linux-amd64.tgz && rm cf-linux-amd64.tgz
+./cf install-plugin -f https://github.com/contraband/autopilot/releases/download/0.0.3/autopilot-linux
+
+./cf login -a $BLUEMIX_API_URL -o $BLUEMIX_ORG -s $BLUEMIX_SPACE -u apikey -p $BLUEMIX_API_KEY
 
 if [ "$1" == "production" ]; then
   BLUEMIX_APP_NAME="discovery-news-demo"
 fi
 
-cf zero-downtime-push $BLUEMIX_APP_NAME -f $BLUEMIX_MAINIFEST_FILE
+./cf zero-downtime-push $BLUEMIX_APP_NAME -f $BLUEMIX_MAINIFEST_FILE
