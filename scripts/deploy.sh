6c86d84b8240cae6e975668aeb1997cfb72a071c
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2017-05-09 14:38:01 -0400

    Fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,10 +10,10 @@ BLUEMIX_SPACE="demos"
 BLUEMIX_APP_NAME="discovery-news-demo-staging"
 BLUEMIX_MAINIFEST_FILE="manifest.$1.yml"
 
-./cf login -a $BLUEMIX_API_URL -o $BLUEMIX_ORG -s $BLUEMIX_SPACE -u apikey -p $BLUEMIX_API_KEY
+cf login -a $BLUEMIX_API_URL -o $BLUEMIX_ORG -s $BLUEMIX_SPACE -u apikey -p $BLUEMIX_API_KEY
 
-if [ "$1" == "production"]; then
+if [ "$1" == "production" ]; then
   BLUEMIX_APP_NAME="discovery-news-demo"
 fi
 
-./cf zero-downtime-push $BLUEMIX_APP_NAME -f $BLUEMIX_MAINIFEST_FILE
+cf zero-downtime-push $BLUEMIX_APP_NAME -f $BLUEMIX_MAINIFEST_FILE
