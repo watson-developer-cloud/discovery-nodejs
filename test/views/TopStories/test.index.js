b16ac4d2304444f1945afed8c5d383ae6e95b248
ZACHARY M. DERNAR <Zachary.M.Dernar@ibm.com>
2017-05-15 13:29:11 -0400

    Cleans up test for Top Stories Sort Select
    Reformats tests, including pulling the selecting functionality
    into separate functions

--- test/views/TopStories/test.index.js
+++ test/views/TopStories/test.index.js
@@ -4,10 +4,16 @@ import sinon from 'sinon';
 import { shallow } from 'enzyme';
 import TopStories from '../../../views/TopStories/index.jsx';
 
+function selectDate(storiesWrapper) {
+  storiesWrapper.find('.sort-option').nodes[0].props.children[1].props.children.props.onChange({target: { value : 'date'}});
+}
+
+function selectRelevance(storiesWrapper) {
+  storiesWrapper.find('.sort-option').nodes[0].props.children[1].props.children.props.onChange({target: { value : 'relevance'}});
+}
+
 describe('<TopStories/>', () => {
-
   let stories = [];
-
   let query_sample = {
     date: {
       'from': '20170301',
@@ -15,32 +21,30 @@ describe('<TopStories/>', () => {
     },
     'text': 'Sample Company'
   };
+  const onSortChangeSpy = sinon.spy();
+  const expected = Object.assign({}, query_sample, {
+    sort: 'date'
+  });
+  let wrapper = shallow(<TopStories stories={stories} query={query_sample} onSortChange={onSortChangeSpy} />);
 
   describe('When I select Date', () => {
-    const onSortChangeSpy = sinon.spy();
+    beforeEach(() => {
+      selectDate(wrapper);
+    });
+
     it('calls onSortChange with Date', () => {
-      const expected = Object.assign({}, query_sample, {
-        sort: 'date'
-      });
-      let wrapper = shallow(<TopStories stories={stories} query={query_sample} onSortChange={onSortChangeSpy} />);
-      wrapper.find('.sort-option').nodes[0].props.children[1].props.children.props.onChange({target: { value : 'date'}});
       assert.ok(onSortChangeSpy.calledWith(expected));
     });
   });
 
   describe('When I select Relevance', () => {
-    const onSortChangeSpy = sinon.spy();
+    beforeEach(() => {
+      selectDate(wrapper);
+      selectRelevance(wrapper);
+    });
+
     it('calls onSortChange with Relevance', () => {
-      const expected = Object.assign({}, query_sample, {
-        sort: 'relevance'
-      });
-      let wrapper = shallow(<TopStories stories={stories} query={query_sample} onSortChange={onSortChangeSpy} />);
-      wrapper.find('.sort-option').nodes[0].props.children[1].props.children.props.onChange({target: { value : 'date'}});
-      wrapper.find('.sort-option').nodes[0].props.children[1].props.children.props.onChange({target: { value : 'relevance'}});
       assert.ok(onSortChangeSpy.calledWith(expected));
     });
   });
-
-  //let firstMention = wrapper.find(Accordion).nodes[0].props.header.props.children[0].props.children;
-
 });
