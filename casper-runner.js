6cd5967444d98856d68ccca42761c39c7da17ec0
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2017-07-18 17:39:01 -0400

    Support webhose news (#41)
    * Support webhose news schema
    
    * Remove environment/collection ID lookup for news
    
    * Fix eslint errors
    
    * Fix tests
    
    * Update credentials to use staging

--- casper-runner.js
+++ casper-runner.js
@@ -17,6 +17,7 @@
 require('dotenv').config();
 
 if (!process.env.DISCOVERY_USERNAME) {
+  // eslint-disable-next-line no-console
   console.log('Skipping integration tests because DISCOVERY_USERNAME is null');
 } else {
   const spawn = require('child_process').spawn;
@@ -24,34 +25,32 @@ if (!process.env.DISCOVERY_USERNAME) {
   const app = require('./app');
   const port = 3000;
 
-  app.then((newsApp) => {
-    const server = newsApp.listen(port, () => {
-      // eslint-disable-next-line no-console
-      console.log('Server running on port: %d', port);
+  const server = app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log('Server running on port: %d', port);
 
-      function kill(code) {
+    function kill(code) {
+      server.close(() => {
+        // eslint-disable-next-line no-process-exit
+        process.exit(code);
+      });
+    }
+
+    function runTests() {
+      const casper = spawn('npm', ['run', 'test-integration']);
+      casper.stdout.pipe(process.stdout);
+
+      casper.on('error', (error) => {
+        // eslint-disable-next-line no-console
+        console.log(`ERROR: ${error}`);
         server.close(() => {
-          // eslint-disable-next-line no-process-exit
-          process.exit(code);
+          process.exit(1);
         });
-      }
+      });
 
-      function runTests() {
-        const casper = spawn('npm', ['run', 'test-integration']);
-        casper.stdout.pipe(process.stdout);
+      casper.on('close', kill);
+    }
 
-        casper.on('error', (error) => {
-          // eslint-disable-next-line no-console
-          console.log(`ERROR: ${error}`);
-          server.close(() => {
-            process.exit(1);
-          });
-        });
-
-        casper.on('close', kill);
-      }
-
-      runTests();
-    });
+    runTests();
   });
 }
