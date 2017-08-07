ea50ea332d9bca0cebaf2b048d90e1288098e714
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2017-08-07 17:38:46 -0400

    Fix start script to utilize clearing of console

--- scripts/start.js
+++ scripts/start.js
@@ -6,9 +6,35 @@ const client = spawn('./node_modules/.bin/react-scripts', ['start'], {
     REACT_APP_SERVER: 'http://localhost:5000',
   }),
 });
+
+let runningAtMessage;
+
 client.stdout.on('data', (data) => {
-  // eslint-disable-next-line no-console
-  console.log(`${data}`);
+  if (data.indexOf('Starting the development server') >= 0) {
+    // eslint-disable-next-line no-console
+    console.log('\033c');
+  }
+
+  if (data.indexOf('You can now view') >= 0) {
+    runningAtMessage = data.toString('utf8');
+  }
+
+  if (data.indexOf('Compiling') >= 0) {
+    // eslint-disable-next-line no-console
+    console.log('\033c');
+  }
+
+  if (data.indexOf('Compiled successfully!') >= 0 && runningAtMessage) {
+    // eslint-disable-next-line no-console
+    console.log('\033c');
+    // eslint-disable-next-line no-console
+    console.log(`${data}`);
+    // eslint-disable-next-line no-console
+    console.log(runningAtMessage);
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(`${data}`);
+  }
 });
 
 client.stderr.on('data', (data) => {
