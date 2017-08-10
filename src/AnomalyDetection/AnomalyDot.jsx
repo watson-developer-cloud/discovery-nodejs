bb89b47b17335ff112051cf89e7a357aaf3bc1c9
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2017-08-10 10:42:14 -0400

    Refactor and style anomaly detection dot (#51)
    * Refactor and style anomaly detection dot
    
    * Fix linting errors

---
+++ src/AnomalyDetection/AnomalyDot.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { string, number, bool, shape } from 'prop-types';
+import { Dot } from 'recharts';
+import AnomalyDetection from './index';
+
+function AnomalyDot({
+  active,
+  dotColor,
+  dotStrokeWidth,
+  activeDotStrokeWidth,
+  ...props
+}) {
+  const hasAnomaly = AnomalyDetection.hasAnomaly(props.payload);
+
+  if (hasAnomaly) {
+    return (
+      <g className="anomaly-dot">
+        <Dot
+          {...props}
+          stroke={dotColor}
+          fill={dotColor}
+          r={6.5}
+        />
+        <Dot
+          {...props}
+          stroke={dotColor}
+          strokeWidth={active ? activeDotStrokeWidth : dotStrokeWidth}
+          fill="none"
+          r={10.5}
+        />
+      </g>
+    );
+  }
+  return <Dot {...props} />;
+}
+
+AnomalyDot.propTypes = {
+  active: bool.isRequired,
+  activeDotStrokeWidth: number.isRequired,
+  dotColor: string.isRequired,
+  dotStrokeWidth: number.isRequired,
+  payload: shape({
+    anomaly: number,
+  }),
+};
+
+AnomalyDot.defaultProps = {
+  active: false,
+  activeDotStrokeWidth: 3,
+  dotColor: '#e2237e',
+  dotStrokeWidth: 1,
+  payload: null,
+};
+
+export default AnomalyDot;
