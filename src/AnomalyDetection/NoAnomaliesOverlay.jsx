8e5038315e6e78f8be80849a078971329009dcab
Andrew Turgeon <andrew.p.turgeon@gmail.com>
2017-08-17 17:12:11 -0400

    Fix tick marks and remove y-axis label for anomaly chart

--- src/AnomalyDetection/NoAnomaliesOverlay.jsx
+++ src/AnomalyDetection/NoAnomaliesOverlay.jsx
@@ -9,7 +9,7 @@ function NoAnomaliesOverlay({ text, onViewData }) {
         {`There are no anomalies in "${text}" for this date range`}
       </h3>
       <h4>
-        Try changing the date range or searching for a different company
+        Try searching for a different company with a recent leadership change or acquisition
       </h4>
       <div className="anomaly-overlay-button-container--div">
         <button type="button" onClick={onViewData}>
