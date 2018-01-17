66142024cf9344d8827dac96b7d4f184e1081abe
Erika Dsouza <erika27desouza@gmail.com>
2018-01-17 10:08:02 -0500

    Move start in Bluemix to point to console (#73)
    * Move start in Bluemix to point to console
    
    * :link: Update link to start in IBM Cloud

--- src/layout.jsx
+++ src/layout.jsx
@@ -16,7 +16,7 @@ export default function Layout() {
         repository="https://github.com/watson-developer-cloud/discovery-nodejs"
         documentation="https://console.bluemix.net/docs/services/discovery/index.html"
         apiReference="http://www.ibm.com/watson/developercloud/discovery/api"
-        startInBluemix="https://console.ng.bluemix.net/registration/?target=/catalog/services/discovery/"
+        startInBluemix="https://console.bluemix.net/registration?target=%2Fdeveloper%2Fwatson%2Fcreate-project%3Fservices%3Ddiscovery%26hideTours%3Dtrue&cm_mmc%3DOSocial_Tumblr-_-Watson%2BCore_Watson%2BCore%2B-%2BPlatform-_-WW_WW-_-wdc-ref%26cm_mmc%3DOSocial_Tumblr-_-Watson%2BCore_Watson%2BCore%2B-%2BPlatform-_-WW_WW-_-wdc-ref%26cm_mmca1%3D000000OF%26cm_mmca2%3D10000409"
         version="GA"
         description="Unlock hidden value in data to find answers, monitor trends and surface patterns, with the world’s most advanced cloud-native insight engine."
       />
