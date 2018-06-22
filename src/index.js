c7efa62a0c443e84c90b1cbaf8fe4a9340149555
German Attanasio <germanattanasio@gmail.com>
2018-06-22 17:56:38 -0400

    chore(analytics): Update the Analytics config

--- src/index.js
+++ src/index.js
@@ -5,16 +5,4 @@ import 'watson-react-components/dist/css/watson-react-components.css';
 import Layout from './layout';
 import './style.css';
 
-function loadAnalytics() {
-  const idaScript = document.createElement('script');
-  idaScript.src = '//www.ibm.com/common/stats/ida_stats.js';
-  document.head.appendChild(idaScript);
-}
-
-if (process.env.REACT_APP_BLUEMIX_ANALYTICS) {
-  // eslint-disable-next-line no-native-reassign, no-underscore-dangle
-  window._analytics = Object.assign({}, window._analytics);
-  window.addEventListener('load', loadAnalytics);
-}
-
 ReactDOM.render(<Layout />, document.getElementById('root'));
