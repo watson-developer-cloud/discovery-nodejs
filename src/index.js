739e24fbdcadee9ae20130c34627e4241e74fc5d
German Attanasio <germanattanasio@gmail.com>
2018-06-25 10:17:50 -0400

    fix(Analytics): move analytics to be loaded async (#101)
    * fix(Test): Update method order to fix lint
    
    * fix(test): force exit for mocha tests
    
    * refactor(analytics): Move scripts to be async
    
    Fix loading the bluemix analytics

--- src/index.js
+++ src/index.js
@@ -5,4 +5,15 @@ import 'watson-react-components/dist/css/watson-react-components.css';
 import Layout from './layout';
 import './style.css';
 
+function loadAnalytics() {
+  const analyticsScript = document.createElement('script');
+  analyticsScript.src = 'https://cdn.rawgit.com/watson-developer-cloud/watson-developer-cloud.github.io/master/analytics.js';
+  document.head.appendChild(analyticsScript);
+}
+
+if (process.env.REACT_APP_BLUEMIX_ANALYTICS) {
+  // eslint-disable-next-line no-native-reassign, no-underscore-dangle
+  window.addEventListener('load', loadAnalytics);
+}
+
 ReactDOM.render(<Layout />, document.getElementById('root'));
