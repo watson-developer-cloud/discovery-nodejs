25ba6c6fd06578dd61a772d4d2a2b1a7794ba02d
German Attanasio <germanattanasio@gmail.com>
2019-11-07 16:06:12 -0500

    chore: remove test-page

--- test/integration/test.mainpage.js
+++ test/integration/test.mainpage.js
@@ -1,18 +1,10 @@
 /* eslint no-undef: 0 */
 /* eslint prefer-arrow-callback: 0 */
-casper.test.begin('Discovery Demo', 3, function suite(test) {
+casper.test.begin('Discovery Demo', 2, function suite(test) {
   const baseHost = 'http://localhost:3000';
-
-  function testForButtons() {
-    casper.waitForSelector('div.query--row._container', function runAfterWaiting() {
-      test.assertExists('input[type=text].text-input--input', 'Company input');
-    });
-  }
-
   casper.start(baseHost, function runAfterStart(result) {
     test.assert(result.status === 200, 'Front page opens');
     test.assertEquals(this.getTitle(), 'Discovery Demo', 'Title is found');
-    testForButtons();
   });
 
   casper.run(function runAfterRun() {
